ce0a6f037817625ab6434c826d7a8880b3578b13
Kirill Chernyshev <kchernyshev@outlook.com>
2025-02-11 20:28:34 +0400

    feat: push db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,17 @@
-import { type Config } from "drizzle-kit";
-
+import { defineConfig } from "drizzle-kit";
+// Import environment variables to be sure that they are loaded
 import { env } from "~/env";
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
   dialect: "singlestore",
+  tablesFilter: ["borsch-drive_*"],
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
   },
-  tablesFilter: ["borsch-storage_*"],
-} satisfies Config;
+});
